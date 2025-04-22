004ad8eb12cbbeb03e93266369cb666b0231dc86
chang0506 <j8970@naver.com>
2025-04-22 11:46:24 +0900

    0422 - jdbc

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -1,17 +1,64 @@
 package main.java;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+        // jdbc:mysql://localhost:3306/jdbc_db
+        // - JDBC 프로토콜:사용하는DBMS://서버주소:포트번호(MySQL 기본 3306)/DB이름
+        final String URL = "jdbc:mysql://localhost:3306/jdbc_db";
+        String user = "root";
+        String password = "ckdgus@7719"; //? 본인의 비밀번호로 변경
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+        Connection conn = null; // DB 연결 객체
+        Statement stmt = null; // DB 실행 객체
+        ResultSet rs = null; // DB 결과 객체
+
+        try {
+            // 1. 드라이버 로딩
+            // : JDBC 드라이버 클래스를 메모리에 로딩
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 2. DB연결
+            // : 실제로 MySQL DB 접속 시도 (성공 시 Connection 객체를 반환)
+            conn = DriverManager.getConnection(URL, user, password);
+            System.out.println("DB 연결 성공");
+
+            // === DB 전체 데이터 조회(READ) === //
+            String sql = "SELECT * FROM member";
+            stmt = conn.createStatement(); // stmt 사용
+            rs = stmt.executeQuery(sql); // SELET 실행
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                System.out.println(id + " | " + name + " | " + email);
+            }
+
+        } catch (Exception e) {
+            System.out.println("DB 연결실패");
+            e.printStackTrace();
+        } finally {
+            // 3. 연결 종료
+            //conn.close();
+            try {
+                if (conn != null && stmt != null && rs != null) {
+                    // DB Connection이 존재한다면 - 자원 누수를 막기 위해 자원 해제를 호출
+                    conn.close();
+                    stmt.close();
+                    rs.close();
+                    System.out.println("DB 연결 종료");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
