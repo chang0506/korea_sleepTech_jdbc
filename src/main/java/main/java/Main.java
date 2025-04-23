16f35c3d80ab7a1b4e3f64ee66a8d5d9768707b3
chang0506 <j8970@naver.com>
2025-04-23 12:36:57 +0900

    0423 - jdbc post

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -1,17 +1,40 @@
 package main.java;
 
-import main.java.manager.MemberManager;
+/*
+ * 1. 회원 CRUD 프로젝트 MEmberManager
+ *   - dao: MemberDao
+ *   - db: DBConnection
+ *   - entity: Member
+ *   - manager: MemberManager
+ *
+ * 2. 게시판 CRUD 프로젝트 (+ MVC 패턴)
+ *  - dao: PostDao
+ *  - db: DBConnection
+ *  - entity: Post
+ *  - service (비즈니스 로직 처리): PostService
+ *  - view: PostView
+ *  - manager: PostManager
+ *
+ *
+ */
 
-import java.sql.SQLException;
+import main.java.manager.PostManager;
 
 public class Main {
     public static void main(String[] args) {
-        MemberManager mm = new MemberManager();
+//        MemberManager mm = new MemberManager();
+        PostManager pm = new PostManager();
 
         try {
-            mm.run();
-        } catch (SQLException e) {
+            pm.run();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+//
+//        try {
+//            mm.run();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 }
