aacf66c6c4191b1505beba5863487dc84c81eaa2
chang0506 <j8970@naver.com>
2025-04-23 09:12:33 +0900

    0423 - gradle

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -1,7 +1,17 @@
 package main.java;
 
+import main.java.manager.MemberManager;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        MemberManager mm = new MemberManager();
 
+        try {
+            mm.run();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/main/java/entity/Member.java
+++ src/main/java/main/java/entity/Member.java
@@ -4,10 +4,7 @@ package main.java.entity;
 // : 데이터베이스 테이블과 매핑되는 클래스
 // >> 1:1
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 // +) Lombok
 //   : Java Domain에서 반복적으로 작성되는 getter나 setter, 생성자 관련 코드 등을
@@ -19,6 +16,10 @@ import lombok.ToString;
 //   3) Enable annotaion 설정
 
 @AllArgsConstructor
+@NoArgsConstructor
+// : JDBC 또는 ORM (DB와 JAVA를 객체화하여 연동하는 체계)에서는
+//  , 빈 객체를 먼저 만들고 setter로 값을 넣는 방식을 사용
+// >> 따라서 AllArgsConstructor도 NoArgsConstructor가 전제되어야 함! (기본 + 전체 필드 생성자)
 @Getter
 @Setter
 @ToString
